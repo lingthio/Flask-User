72a17ec7cdf0684f7421f9d9dad05d72d8ae8004
Ling Thio <ling.thio@gmail.com>
2014-04-15 11:49:09 -0700

    v0.4.9: login_or_register.html

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ from setuptools import setup
 
 setup(
     name='Flask-User',
-    version='0.4.8',
+    version='0.4.9',
     url='http://github.com/lingthio/Flask-User',
     license='BSD License',
     author='Ling Thio',
