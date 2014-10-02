c329dc1f56e1b0d0333f1d8cf5e2190053a028df
Ling Thio <ling.thio@gmail.com>
2014-10-01 22:07:06 -0700

    version

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ from setuptools import setup
 
 setup(
     name='Flask-User',
-    version='0.5.2',
+    version='0.5.3',
     url='http://github.com/lingthio/Flask-User',
     license='BSD License',
     author='Ling Thio',
