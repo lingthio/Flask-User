866b033f944e13d993704479fcca01dcabd2eba1
Ling Thio <ling.thio@gmail.com>
2014-10-03 00:24:10 -0700

    Added Python 3.3 and 3.4

--- setup.py
+++ setup.py
@@ -113,6 +113,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Security',
         'Topic :: Software Development :: Libraries :: Python Modules',
