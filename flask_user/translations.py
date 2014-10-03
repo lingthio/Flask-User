3110905f7b5cff5eabc44c60a0c2c0486acb6950
Ling Thio <ling.thio@gmail.com>
2014-10-03 16:00:07 -0700

    Improved Flask-Babel detection.

--- flask_user/translations.py
+++ flask_user/translations.py
@@ -1,82 +1,76 @@
-""" This module contains functions to translate strings for Flask-User.
-    The functions are based on Flask-Babel.
-
-    It redefines flask_babel.get_translations() to change the following:
-    - look for translations first in the app.root_dir, then in the flask_user dir
-    - change the domain from 'messages' to 'flask_user'
+""" This file contains functions to translate strings for Flask-User.
 
     :copyright: (c) 2013 by Ling Thio
     :author: Ling Thio (ling.thio@gmail.com)
     :license: Simplified BSD License, see LICENSE.txt for more details."""
 
-import gettext as python_gettext
+from flask import _request_ctx_stack, current_app
 
-# Flask-User can be used with or without Flask-Babel and Babel
-try:
-    from flask.ext.babel import get_locale, support
+# To avoid requiring the Flask-Babel, Babel and speaklater packages,
+# we check if the app has initialized Flask-Babel or not
+def get_translations():
+    # If there is no context:  return None
+    ctx = _request_ctx_stack.top
+    if not ctx:
+        return None
 
-    # Flask-Babel is installed
+    # If context exists and contains a cashed value, return cached value
+    translations = getattr(ctx, 'flask_user_translations', None)
+    if translations:
+        return translations
+
+    # If App has not initialized Flask-Babel: return None
+    app_has_initalized_flask_babel = 'babel' in current_app.extensions
+    if not app_has_initalized_flask_babel:
+        return None
+
+    # Prepare search properties
     import os
-    from flask import _request_ctx_stack, current_app, render_template
+    import gettext as python_gettext
+    from flask.ext.babel import get_locale, support
+    domain = 'flask_user'
+    locales = [get_locale()]
+    languages = [str(locale) for locale in locales]
 
-    def get_translations():
-        """ Search the Application directory and the Flask-User directory for the
-            Flask-User translations file and return a Translations() object."""
-        ctx = _request_ctx_stack.top
-        if ctx is None:
-            return None
-        translations = getattr(ctx, 'flask_user_translations', None)
-        if translations is None:
-            # Prepare settings
-            domain = 'flask_user'
-            locales = [get_locale()]
-            languages = [str(locale) for locale in locales]
-            # Search Application directory
-            app_dir = os.path.join(current_app.root_path, 'translations')
-            filename = python_gettext.find(domain, app_dir, languages)
-            if filename:
-                translations = support.Translations.load(app_dir, locales, domain=domain)
-            else:
-                # Search Flask-User directory
-                flask_user_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'translations')
-                translations = support.Translations.load(flask_user_dir, locales, domain=domain)
+    # See if translations exists in Application dir
+    app_dir = os.path.join(current_app.root_path, 'translations')
+    filename = python_gettext.find(domain, app_dir, languages)
+    if filename:
+        translations = support.Translations.load(app_dir, locales, domain=domain)
 
-            ctx.flask_user_translations = translations
+    # See if translations exists in Flask-User dir
+    else:
+        flask_user_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'translations')
+        translations = support.Translations.load(flask_user_dir, locales, domain=domain)
 
-        return ctx.flask_user_translations
+    # Cache result in context
+    ctx.flask_user_translations = translations
 
-    def gettext(string, **variables):
-        """ Translate specified string."""
-        translations = get_translations()
-        if not translations:
-            return string % variables
+    return translations
+
+def gettext(string, **variables):
+    """ Translate specified string."""
+    translations = get_translations()
+    if translations:
         return translations.ugettext(string) % variables
+    return string % variables
 
-    def ngettext(singular, plural, num, **variables):
-        """ Translate a singular/plural string based on the number 'num'."""
-        translations = get_translations()
-        variables.setdefault('num', num)
-        if not translations:
-            return (singular if num == 1 else plural) % variables
+def ngettext(singular, plural, num, **variables):
+    """ Translate a singular/plural string based on the number 'num'."""
+    translations = get_translations()
+    variables.setdefault('num', num)
+    if translations:
         return translations.ungettext(singular, plural, num) % variables
+    return (singular if num == 1 else plural) % variables
 
-    def lazy_gettext(string, **variables):
-        """ Similar to 'gettext' but the string returned is lazy which means
-            it will be translated when it is used as an actual string."""
+def lazy_gettext(string, **variables):
+    """ Similar to 'gettext' but the string returned is lazy which means
+        it will be translated when it is used as an actual string."""
+    try:
         from speaklater import make_lazy_string
         return make_lazy_string(gettext, string, **variables)
-
-    print('translations is using Flask-Babel')
-
-except ImportError:
-    # Flask-Babel has not been installed
-    def gettext(string, **variables):
-        return python_gettext.gettext(string, **variables)
-
-    def lazy_gettext(string, **variables):
-        return python_gettext.gettext(string, **variables)
-
-    print('translations is using python gettext')
+    except ImportError:
+       return string % variables
 
 _ = lazy_gettext
 _home_page = _('Home Page')
