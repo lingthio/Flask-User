7a1cd837b1b188701cbb19c6f86980f342d5e4f1
Ling Thio <ling.thio@gmail.com>
2014-10-21 19:43:26 -0700

    docs

--- example_apps/basic_app.py
+++ example_apps/basic_app.py
@@ -1,5 +1,4 @@
 from flask import Flask, redirect, render_template_string, request, url_for
-from flask.ext.babel import Babel
 from flask.ext.mail import Mail
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.user import current_user, login_required, UserManager, UserMixin, SQLAlchemyAdapter
@@ -7,51 +6,30 @@ from flask.ext.user import current_user, login_required, UserManager, UserMixin,
 # Use a Class-based config to avoid needing a 2nd file
 class ConfigClass(object):
     # Configure Flask
-    SECRET_KEY = 'THIS IS AN INSECURE SECRET'               # Change this for production!!!
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///basic_app.sqlite'  # Use Sqlite file db
+    SECRET_KEY = 'THIS IS AN INSECURE SECRET'                     # Change this for production!!!
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///single_file_app.sqlite'  # Use Sqlite file db
     CSRF_ENABLED = True
 
     # Configure Flask-Mail -- Required for Confirm email and Forgot password features
-    MAIL_SERVER   = 'smtp.gmail.com'
-    MAIL_PORT     = 465
-    MAIL_USE_SSL  = True                            # Some servers use MAIL_USE_TLS=True instead
     MAIL_USERNAME = 'email@example.com'
     MAIL_PASSWORD = 'password'
     MAIL_DEFAULT_SENDER = '"Sender" <noreply@example.com>'
+    MAIL_SERVER   = 'smtp.gmail.com'
+    MAIL_PORT     = 465
+    MAIL_USE_SSL  = True                            # Some servers use MAIL_USE_TLS=True instead
 
-    # Configure Flask-User
-    USER_PRODUCT_NAME           = "ProductName"     # Used by email templates
-    USER_ENABLE_USERNAME        = True              # Register and Login with username
-    USER_ENABLE_EMAIL           = True              # Register and Login with email
-    USER_LOGIN_TEMPLATE         = 'flask_user/login_or_register.html'
-    USER_REGISTER_TEMPLATE      = 'flask_user/login_or_register.html'
-    USER_AFTER_LOGIN_ENDPOINT   = 'profile_page'
-    USER_AFTER_CONFIRM_ENDPOINT = 'profile_page'
-
-def create_app(test_config=None):                   # For automated tests
-    # Setup Flask and read config from ConfigClass defined above
+def create_app():
+    """ Flask application factory """
+    
+    # Setup Flask app and app.config
     app = Flask(__name__)
     app.config.from_object(__name__+'.ConfigClass')
 
-    # Load local_settings.py if file exists         # For automated tests
-    try: app.config.from_object('local_settings')
-    except: pass
-
-    # Load optional test_config                     # For automated tests
-    if test_config:
-        app.config.update(test_config)
-
     # Initialize Flask extensions
-    babel = Babel(app)                              # Initialize Flask-Babel
     db = SQLAlchemy(app)                            # Initialize Flask-SQLAlchemy
     mail = Mail(app)                                # Initialize Flask-Mail
 
-    @babel.localeselector
-    def get_locale():
-        translations = [str(translation) for translation in babel.list_translations()]
-        return request.accept_languages.best_match(translations)
-
-    # Define User model. Make sure to add flask.ext.user UserMixin!!
+    # Define User model. Make sure to add flask.ext.user UserMixin !!!
     class User(db.Model, UserMixin):
         id = db.Column(db.Integer, primary_key=True)
         active = db.Column(db.Boolean(), nullable=False, default=False)
@@ -65,8 +43,8 @@ def create_app(test_config=None):                   # For automated tests
     db.create_all()
 
     # Setup Flask-User
-    db_adapter = SQLAlchemyAdapter(db,  User)       # Select database adapter
-    user_manager = UserManager(db_adapter, app)     # Init Flask-User and bind to app
+    db_adapter = SQLAlchemyAdapter(db, User)        # Register the User model
+    user_manager = UserManager(db_adapter, app)     # Initialize Flask-User
 
     # The Home page is accessible to anyone
     @app.route('/')
@@ -74,41 +52,37 @@ def create_app(test_config=None):                   # For automated tests
         return render_template_string("""
             {% extends "base.html" %}
             {% block content %}
-                <h2>{%trans%}Home Page{%endtrans%}</h2>
+                <p><a href="{{ url_for('home_page') }}">Home page</a> |
+                   <a href="{{ url_for('profile_page') }}">Profile page</a></p>
+
+                <h2>Home page</h2>
+
                 {% if current_user.is_authenticated() %}
-                <p> <a href="{{ url_for('profile_page') }}">
-                    {%trans%}Profile Page{%endtrans%}</a></p>
-                <p> <a href="{{ url_for('user.logout') }}">
-                    {%trans%}Sign out{%endtrans%}</a></p>
+                    <p><a href="{{ url_for('user.logout')  }}">Sign out</a></p>
                 {% else %}
-                <p> <a href="{{ url_for('user.login') }}">
-                    {%trans%}Sign in or Register{%endtrans%}</a></p>
+                    <p><a href="{{ url_for('user.login') }}">Sign in</a></p>
                 {% endif %}
             {% endblock %}
             """)
-        if current_user.is_authenticated():
-            return redirect(url_for('profile_page'))
-        else:
-            return redirect(url_for('user.login'))
 
-# The Profile page requires a logged-in user
+    # The Profile page requires a logged-in user
     @app.route('/profile')
     @login_required                                 # Use of @login_required decorator
     def profile_page():
         return render_template_string("""
             {% extends "base.html" %}
             {% block content %}
-                <h2>{%trans%}Profile Page{%endtrans%}</h2>
-                <p> {%trans%}Hello{%endtrans%}
-                    {{ current_user.username or current_user.email }},</p>
-                <p> <a href="{{ url_for('home_page') }}">
-                    {%trans%}Home Page{%endtrans%}</a></p>
-                <p> <a href="{{ url_for('user.change_username') }}">
-                    {%trans%}Change username{%endtrans%}</a></p>
-                <p> <a href="{{ url_for('user.change_password') }}">
-                    {%trans%}Change password{%endtrans%}</a></p>
-                <p> <a href="{{ url_for('user.logout') }}">
-                    {%trans%}Sign out{%endtrans%}</a></p>
+                <p><a href="{{ url_for('home_page') }}">Home page</a> |
+                   <a href="{{ url_for('profile_page') }}">Profile page</a></p>
+
+                <h2>Profile page</h2>
+
+                <p>Username: {{ current_user.username }}</p>
+                <p>Email:    {{ current_user.email }}   </p>
+
+                <p><a href="{{ url_for('user.change_username') }}">Change username</a></p>
+                <p><a href="{{ url_for('user.change_password') }}">Change password</a></p>
+                <p><a href="{{ url_for('user.logout') }}">Sign out</a></p>
             {% endblock %}
             """)
 
