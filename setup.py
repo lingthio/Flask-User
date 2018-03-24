09a84b0aa0fe7dab45c4fbc9f79ca2e77778df7c
Ling Thio <ling.thio@gmail.com>
2018-03-23 17:38:02 -0700

    version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import sys
 
-__version__ = '0.6.19'
+__version__ = '0.6.20'
 
 # Load pytest and pytest-runner only when needed:
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
