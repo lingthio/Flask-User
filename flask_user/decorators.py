5f97b54c3d6467dbc99e797481d86fcfde06b37a
Jason Hines <jason@hinesnetwork.com>
2019-01-15 13:09:36 -0500

    refresh login working

--- flask_user/decorators.py
+++ flask_user/decorators.py
@@ -60,38 +60,6 @@ def login_required(view_function):
     return decorator
 
 
-def fresh_login_required(view_function):
-    """ This decorator ensures that the current user is logged in.
-
-    Example::
-
-        @route('/member_page')
-        @login_required
-        def member_page():  # User must be logged in
-            ...
-
-    If USER_ENABLE_EMAIL is True and USER_ENABLE_CONFIRM_EMAIL is True,
-    this view decorator also ensures that the user has a confirmed email address.
-
-    | Calls unauthorized_view() when the user is not logged in
-        or when the user has not confirmed their email address.
-    | Calls the decorated view otherwise.
-    """
-    @wraps(view_function)    # Tells debuggers that is is a function wrapper
-    def decorator(*args, **kwargs):
-        user_manager = current_app.user_manager
-        
-        # User must be logged in with a confirmed email address
-        allowed = _is_logged_in_with_confirmed_email(user_manager)
-        if not allowed:
-            # Redirect to unauthenticated page
-            return user_manager.unauthenticated_view()
-
-        # It's OK to call the view
-        return view_function(*args, **kwargs)
-
-    return decorator
-
 def roles_accepted(*role_names):
     """| This decorator ensures that the current user is logged in,
     | and has *at least one* of the specified roles (OR operation).
